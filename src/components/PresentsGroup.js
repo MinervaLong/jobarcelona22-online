9ed57af4bdcee3a6082a7c3bbbc4484943a26252
MinervaLong <vanesa.perea.cuevas@gmail.com>
2022-05-16 10:59:21 +0200

    Add HTML tags & BEM classes

--- src/components/PresentsGroup.js
+++ src/components/PresentsGroup.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const PresentsGroup = () => {
   return (
-    <div>PresentsGroup</div>
+    <section className='group'>
+      <h2 className='group__title'>Title</h2>
+      <p className='group__description'>Description</p>
+      <div className='group__tagsContainer'><button className='group__tag'>Tag</button></div>
+    </section>    
   )
 }
 
