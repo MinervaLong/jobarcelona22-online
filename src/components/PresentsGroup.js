2dc33b8bd588a62a5d4cc566e852e485ae6425e9
MinervaLong <vanesa.perea.cuevas@gmail.com>
2022-05-16 12:01:22 +0200

    Add basic structure to PresentsGroup component

--- src/components/PresentsGroup.js
+++ src/components/PresentsGroup.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import './../styles/sass/App.scss';
 
 const PresentsGroup = () => {
   return (
     <section className='group'>
       <h2 className='group__title'>Title</h2>
       <p className='group__description'>Description</p>
-      <div className='group__tagsContainer'><button className='group__tag'>Tag</button></div>
+      <div className='group__tagsContainer'>
+        <button className='group__tag'> Deportes de riesgo</button>
+        <button className='group__tag'> Libros</button></div>
     </section>    
   )
 }
