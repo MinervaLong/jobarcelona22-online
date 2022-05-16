005120d66a5df38f1ce51a56712c291ebcdc80b5
MinervaLong <vanesa.perea.cuevas@gmail.com>
2022-05-16 19:26:37 +0200

    Add logic to consume & display json data

--- src/components/PresentsListView.js
+++ src/components/PresentsListView.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import PresentsGroup from './PresentsGroup'
+import groups from './../api/PresentsData.json'
 
 const PresentsListView = () => {
   return (
     <section className='listView'>
       <h1 className='listView__title'>Presents Management App</h1>
       <section className='listView__groupsContainer'>
-        <PresentsGroup />
+        {groups.map(group =>(
+          <PresentsGroup 
+          title={group.title}
+          icon={group.emoji}
+          description={group.description}
+          tags={group.tags}
+          />
+        ))}
+        
       </section>
     </section>
   )
