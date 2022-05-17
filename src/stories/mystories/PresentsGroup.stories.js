1cfc2a38dd78450f0eae5021f27af592ad0ea844
MinervaLong <vanesa.perea.cuevas@gmail.com>
2022-05-18 00:26:10 +0200

    Add PresentsGroup story

---
+++ src/stories/mystories/PresentsGroup.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PresentsGroup from '../../components/PresentsGroup';
+
+export default {
+    title: "PresentsGroup",
+    component: PresentsGroup,
+    argTypes: {handleClick: {action: "Undefined function"}},
+}
+
+const Template = (args) => <PresentsGroup {...args} />
+
+
+export const Group = Template.bind({})
+Group.args = {
+    border: "1px solid rgb(48, 13, 87)",
+    borderRadius: "5px", 
+    margin: "2%", 
+    padding: "3%",
+    width: "90%", 
+}
+
