52a67cf66892ca8202f21223f4873a997b6cda6a
MinervaLong <vanesa.perea.cuevas@gmail.com>
2022-05-18 00:24:50 +0200

    Refractor PresentsListViews

--- src/components/PresentsListView.js
+++ src/components/PresentsListView.js
@@ -3,21 +3,20 @@ import PresentsGroup from './PresentsGroup'
 import groups from './../api/PresentsData.json'
 
 const PresentsListView = () => {
+  const presentsGroup = groups.map(group =>(
+      <PresentsGroup
+      key={group.id} 
+      title={group.title}
+      emo={group.emoji}
+      description={group.description}
+      tags={group.tags}
+      />
+    ))
+    
   return (
     <section className='listView'>
       <h1 className='listView__title'>Presents Management App</h1>
-      <section className='listView__groupsContainer'>
-        {groups.map(group =>(
-          <PresentsGroup
-          key={group.id} 
-          title={group.title}
-          emo={group.emoji}
-          description={group.description}
-          tags={group.tags}
-          />
-        ))}
-        
-      </section>
+      <section className='listView__groupsContainer'>{presentsGroup}</section>
     </section>
   )
 }
