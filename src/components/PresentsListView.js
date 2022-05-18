f1f8e83e23d9d33261241548c034ba74ff0b8a00
MinervaLong <vanesa.perea.cuevas@gmail.com>
2022-05-18 19:16:36 +0200

    Add custom hook for responisveness

--- src/components/PresentsListView.js
+++ src/components/PresentsListView.js
@@ -2,8 +2,14 @@ import React from 'react';
 import PresentsGroup from './PresentsGroup';
 import groups from './../api/PresentsData.json';
 import HeaderListView from './HeaderListView';
+import useViewPort from '../hooks/UseViewPort';
+
 
 const PresentsListView = () => {
+  // RESPONSIVE LAYOUT
+  const { width } = useViewPort();
+  const breakpoint = 801;
+
   const presentsGroup = groups.map(group =>(
       <PresentsGroup
       key={group.id} 
@@ -17,7 +23,12 @@ const PresentsListView = () => {
   return (
     <section className='listView'>
       <HeaderListView />
-      <section className='listView__groupsContainer'>{presentsGroup}</section>
+      {width < breakpoint ? <section className='listView__groupsContainer'>{presentsGroup}</section>
+      :
+      <section className='listView__groupsContainer--horizontal'>
+        <div className='listView__groupsContainer--scroller'>{presentsGroup}</div>        
+      </section>
+      }      
     </section>
   )
 }
